08c694c84733b55bfec7f2b9c0ec5b25eef2a288
Emerson Junior <emersonjr02@gmail.com>
2024-07-23 14:32:03 -0300

    feat: implementing the first things to the clientUser

--- client/ClientUser.py
+++ client/ClientUser.py
@@ -2,14 +2,59 @@ import socket
 
 PORT = 3300
 
-class ClientUser:
+mensagemNaoEncontrouUser = "Usuario nao esta no servidor"
+mensagemUnauthorized = "Você não está autorizado a fazer isso"
+mensagemGroupNameUsed = "Nome de Grupo já existe"
+mensagemExistsUserEmail = "Já existe usuario com esse email"
 
-    users = dict() # dict<Email, sock> ? (I guess)
-    
+class ClientUser:
     # Isso aqui seria interessante pra encapsular melhor
 
-    def __init__(self, _name, _email):
-        pass
+    def __init__(self, _name, _email, _passw, _cep):
+
+        self.name, self.email, self.passw, self.cep = _name, _email, _passw, _cep
+
+        # definitions of the client socket
+
+        self.sockUser = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # socket definitions done
+        # this is the first connection so we need to
+        # sign up this big person
+
+        self.sockUser.connect(socket.gethostbyname(), PORT)
+
+        mensagem = "0|" + _email + "|" + _name + "|" + _passw + "|" + _cep + "|"
+
+        self.sockUser.send(mensagem)
+
+        # A gente tem q fazer close aqui né?
+
+        self.sockUser.close()
+    
+    def getName(self):
+        return self.name
+    
+    def getEmail(self):
+        return self.email
+    
+    def addFriend(self, who):
+
+        # who é um email
+
+        self.sockUser.connect(socket.gethostbyname(), PORT)
+
+        mensagem = "4|" + who + '|'
+
+        self.sockUser.send(mensagem.encode("utf-32"))
+
+        self.sockUser.close()
+
+    def rcvServer(self):
+
+        mensagemSrvr = self.sockUser.rcv(1024).decode("utf-32")
+
+        # Tratar as mensagens de acordo com o estabelecido pelo servidor
+        
 
 
 
