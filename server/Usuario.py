4e0c69bacc05a4161bd9344d242aad50c7546e10
Emerson Junior <emersonjr02@gmail.com>
2024-07-27 13:27:30 -0300

    fix: refactorating the server code

--- server/Usuario.py
+++ server/Usuario.py
@@ -1,76 +1,192 @@
 from socket import *
 from Grupo import Grupo
+import threading
 mensagemNotFoundUser = "canal com o usuario nao encontrado"
 mensagemNotFoundGrupo = "voce nao esta no grupo"
+mensagemUnauthorized = "Você não está autorizado a fazer isso"
+mensagemGroupNameUsed = "Nome de Grupo já existe"
 mensagemOutGrupo = "5@"
 
 class Usuario:
 
-    grupos = list()
     users = list()
+    groups = list()
+    usersChannel = dict() # dict<email, msgs>
+    groupsChannel = dict() # dict<nome, msgs>
 
     # seria interessante ter outra lista com outros usuarios
     # os quais ele já iniciou uma conversa? Acho q essa lista é
     # algo já implementado naquele exemplo de IRC, link:
     # https://github.com/Gabrielcarvfer/Redes-de-Computadores-UnB/blob/master/trabalhos/20181/Lab2/ExemploIRC.py
 
-    def __init__(self, name, email, passw, cep, ipv4, sockUser) -> None:
+    def __init__(self, name, email, passw, cep, ipv4, sockUser, server) -> None:
 
         self.name, self.email, self.passw, self.cep, self.ipv4 = name, email, passw, cep, ipv4
+        self.serv = server
 
         # Sockets definitions are already done in the server
 
+        self.conected = ""
+        self.tipoConec = -1
         self.sockUser = sockUser 
     
-    def receiveMsgUser(self, message):
+    def receiveMsgUser(self, message, whoSent):
 
         # Devolvemos essa mensagem pro nosso usuario
 
-        self.sockUser.send(message.encode("utf-32"))
+        self.usersChannel[whoSent].append(message)
+        # tipo conec = 1 - > Usuario
+        if (self.tipoConec == 1 and self.conected == whoSent):
+            self.sockUser.send(message.encode("utf-32"))
     
-    def receiveMsgGrupo(self, message):
+    def receiveMsgGrupo(self, message, whoSent):
 
         # vc está garantidamente no grupo
         # Devolvemos essa mensagem pro nosso usuario
-
-        self.sockUser.send(message.encode("utf-32"))
+        self.groupsChannel[whoSent].append(message)
+        # tipo conec = 2 - > grupo
+        if (self.tipoConec == 2 and self.conected == whoSent):
+            self.sockUser.send(message.encode("utf-32"))
     
     def sendMsgToUser(self, message, dest):
 
         # procuramos se estamos mandando msg pra algum usuario
         # com algum canal
 
-        for user in self.users:
+        for user in self.users.keys():
 
             if(dest == user.getName()):
-
+                
+                self.usersChannel[user.getEmail()].append(message)
                 return list(message, user)
-        
-        return list(mensagemNotFoundUser, "NF")
     
     def sendMsgToGroup(self, message, dest):
 
         # procuramos se estamos mandando mensagem para algum grupo
         # com algum canal já existente
-
-        for grupo in self.grupos:
+        for grupo in self.grupos.keys():
 
             if(dest == grupo.getName()):
 
+                self.groupsChannel[grupo.getName()].append(message)
                 return list(message, grupo)
-        
-        return list(mensagemNotFoundGrupo, "NF")
     
     def start(self):
-        
-       while (1):
-           
-           mensagem = self.sockUser.recv(1024).decode("utf-32")
 
-           if (mensagem == ""):
-               continue
+       while True:
            
+            mensagem = self.sockUser.recv(1024).decode("utf-32")
 
+            if (mensagem == ""):
+                continue
+           
+            """
+                0 -> open connection
+                1 -> close
+                2 -> message for group/direct
+                3 -> logout
+                4 -> quer adicionar um novo usuario
+                5 -> manda convite
+                6 -> pede pra entrar
+                7 -> entra
+                8 -> cria grupo
+                9 -> sai Grupo
+                10 -> pede Users
+                11 -> pede Groups
+                0|tipo|email ou nome
+                1|
+                4|emailDoNewUser
+                5|grupo|email
+                6|grupo|email
+                7|grupo|email
+                8|grupo|email
+                9|NomeGrupo|email
+           """
+
+            message = mensagem.split("|")
+
+            match message[0]:
+            
+                case ('1'):
+                    self.conected = ""
+                    self.tipoConec = -1
+                case ('0'):
+
+                    mensagemGrl = ""
+                    if (int(message[1]) == 1):
+                        for msg in self.usersChannel[message[2]]:
+                            mensagemGrl += f"{msg}|"
+                    else:
+                        for msg in self.groupssChannel[message[2]]:
+                            mensagemGrl += f"{msg}|"
+                    
+                    self.sockUser.send(mensagemGrl.encode("utf-32"))
+                    
+                    self.conected = message[2]
+                    self.tipoConec = int(message[1])
+                case ('2'):
+                    self.serverRcv(message[1])
+                case ('3'):
+                    break
+                case ('4'):
+    
+                    # Adiciona um novo usuario para o usuario atual
+                    # precisa de threading aqui? acho q já começa a ficar muito muito
+                    t = threading.Thread(target= self.addUser, args=(self.serv.users[message[1]]))
+                    t1 = threading.Thread(target= self.serv.users[message[1]].addUser, args=(self))
+                    t1.start()
+                    t.start()
+                case('5'):
+                    if (self.name != self.serv.groups[message[1]].getAdmin()):
+                        self.sockUser.send(mensagemUnauthorized.encode("utf-32"))
+                        continue
+
+                    # O usuario devera receber o grupo que foi convidado
+                    t = threading.Thread(target= self.serv.users[message[2]].rcvInvite, args=(message[1]))
+                    t.start()
+                    
+                case('6'):
+
+                    t = threading.Thread(target= (self.serv.groups[message[1]].getAdmin()).pedidoParaEntrar, args=(message[2]))
+                    t.start()
+
+                case('7'):
+                    t = threading.Thread(target= self.serv.groups[message[1]].addUser, args=(self.users[message[2]]))
+                    t1 = threading.Thread(target= self.serv.users[message[2]].addGroup, args=(self.groups[message[1]]))
+                    t1.start()
+                    t.start()
+                
+                case('8'):
+
+                    if (message[1] in self.serv.groups.keys()):
+                        self.sockUser.send(mensagemGroupNameUsed.encode("utf-32"))
+                        continue
+
+                    newGrupo = Grupo(message[1], self.serv.users[message[2]])
+
+                    self.serv.groups[message[1]] = newGrupo
+
+                    t = threading.Thread(target= self.serv.users[message[2]].addGroup, args=(newGrupo))
+                    t.start()
+                
+                case('9'):
+
+                    t = threading.Thread(target= self.serv.groups[message[1]].eraseUser, args=(self.serv.users[message[2]]))
+                    t1 = threading.Thread(target= self.serv.users[message[2]].sairDeUmGrupo, args=(self.serv.groups[message[1]]))
+                    t1.start()
+                    t.start()
+                case('10'):
+                    groupsGrl = ""
+                    for grupo in self.serv.groups.keys():
+                        groupsGrl += grupo
+                    
+                    self.sockUser.send(groupsGrl.encode("utf-32"))
+                case('11'):
+                    usersGrl = ""
+                    for user in self.serv.users.keys():
+                        usersGrl += user
+                    
+                    self.sockUser.send(usersGrl.encode("utf-32"))
 
 
     def serverRcv(self, mensagem):
@@ -89,24 +205,13 @@ class Usuario:
 
             content, dest = self.sendMsgToGroup(mensagem, mensagemSplitada[2])
 
-            if (content == mensagemNotFoundGrupo):
-
-                self.sockUser.send(content.encode("utf-32"))
-                return
-            
             dest.rcvAndPropMsg(content)
             
             return
 
         content, dest = self.sendMsgToUser(mensagem, mensagemSplitada[2])
-
-        if (content == mensagemNotFoundUser):
-
-            self.sockUser.send(content.encode("utf-32"))
-
-            return
         
-        dest.receiveMsgUser(content)
+        dest.receiveMsgUser(content, self.getEmail())
 
     # The main idea in this two methods is to
     # make the process of creating new chanels more
@@ -118,6 +223,7 @@ class Usuario:
 
         mensagem = "6@" + userStuff.getName() + "@" + userStuff.getEmail() + '@'
 
+        self.usersChannel[userStuff.getEmail()] = list()
         self.sockUser.send(mensagem.encode("utf-32"))
 
         return
@@ -156,6 +262,7 @@ class Usuario:
         mensagem = "7@" + groupStuff + "@"
         
         self.sockUser(mensagem.encode("utf-32"))
+        self.groupsChannel[groupStuff.getName()] = list()
         self.grupos.append(groupStuff)
         return
     
--- server/main.py
+++ server/main.py
@@ -6,16 +6,14 @@ import threading
 
 HOST = "127.0.0.1"
 PORT = 3300
-mensagemNaoEncontrouUser = "Usuario nao esta no servidor"
-mensagemUnauthorized = "Você não está autorizado a fazer isso"
-mensagemGroupNameUsed = "Nome de Grupo já existe"
+
 mensagemExistsUserEmail = "Já existe usuario com esse email"
 
 class Server: 
     def __init__(self):
 
         self.users = dict() # dict<email, user>
-        self.groups = list()
+        self.groups = dict() # dict<nome, group>
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((HOST, PORT))
@@ -35,7 +33,7 @@ class Server:
 
                 client.send(mensagem.encode("utf-32"))
 
-                t = threading.Thread(target= self.user.start(), args=())
+                t = threading.Thread(target= user.start(), args=())
                 t.start()
                 return
         
@@ -44,10 +42,10 @@ class Server:
 
     def sign_up(self, message, client, address):
 
-        user = Usuario(message[2], message[1], message[3], message[4], address, client)
-        t = threading.Thread(target= self.user.start(), args=())
-        t.start()
+        user = Usuario(message[2], message[1], message[3], message[4], address, client, self)
         self.users[user.getEmail()] = user
+        t = threading.Thread(target= user.start(), args=())
+        t.start()
 
     def receive(self):
 
@@ -57,29 +55,12 @@ class Server:
             """
                 0 -> signup
                 1 -> login
-                2 -> message for group/direct
-                3 -> logout
-                4 -> quer adicionar um novo usuario
-                5 -> manda convite
-                6 -> pede pra entrar
-                7 -> entra
-                8 -> cria grupo
-                9 -> sai Grupo
+    
                 0|email|name|password|cep
                 1|email|password
-                4!emailDoNewUser
-                5|grupo|email
-                6|grupo|email
-                7|grupo|email
-                8|grupo|email
-                9|NomeGrupo|email
                 
                 Acredito q seria interessante deixar a resposta de um convite ou pedido
                 privadas
-
-                Futuramente com o lado do cliente implementado e quando formos olhar as coisas
-                da transmissão de audio e video mais coisas devem ser adicionadas, mas
-                o corpo acho q é isso
 
             """
             message = client.recv(1024).decode("utf-32")
